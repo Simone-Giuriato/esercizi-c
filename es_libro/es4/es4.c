eafa996f473ec1220046e36ccfdf4f388c1ba520
Simone-Giuriato <72790964+Simone-Giuriato@users.noreply.github.com>
2021-12-01 16:34:05 +0100

    Aggiunta dell'esercizio 4 e documentazione con dosygen

--- es_libro/es4/es4.c
+++ es_libro/es4/es4.c
@@ -1,3 +1,9 @@
+
+/// @file       es4.c
+/// @brief      file dell'esercizio 4 del libro pdf
+/// @date       26/11/2021
+/// @author     Giuriato Simone
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,10 +12,19 @@
 #include <unistd.h>
 #include <ctype.h>
 
+/**
+* @brief    struct Persona_T: struct formata da un nome e un cognome
+* @param    char nome[10]: nome della persona
+* @param    char cognome[10]: cognome della persona
+*/
 typedef struct{
     char nome[10];
     char cognome[10];
 }Persona_T;
+/**
+* @fn       Persona_T Inserisci()
+* @brief    funzione che permette l'inserimento delle credenziali della persona dall'utente
+*/
 Persona_T Inserisci()
 {
     Persona_T persona;
@@ -20,6 +35,11 @@ Persona_T Inserisci()
     return persona;
 
 }
+/**
+* @fn       void Trasformazione(char stringa[100])
+* @brief    funzione che permette di trasformare la prima lettara in maiuscolo e tutte le altre in minuscolo
+* @param    char stringa[100]: stringa da convertire
+*/
 void Trasformazione(char stringa[100])
 {
     int i;
@@ -35,6 +55,11 @@ void Trasformazione(char stringa[100])
         }
     }
 }
+/**
+* @fn       void Maiuscolo(char stringa[100])
+* @brief    funzione che permette di convertite tutta la stringa in maiuscolo
+* @param    char stringa[100]: stringa da convertire
+*/
 void Maiuscolo(char stringa[100]){
     int i;
     for(i=0; i<strlen(stringa);i++)
@@ -46,6 +71,10 @@ void Maiuscolo(char stringa[100]){
     }
 
 }
+/**
+* @fn       int main()
+* @brief    funzione principale, istanzia un processo figlio e il padre permette di leggere un nome ed un cognome, che verranno poi convertiti con la funzione Trasformazione e scritti in un file. Il file verra' poi eliminato (con remove()). Il figlio contemporaneamente legge quello che c'e' dentro il file e lo stampa a schermo, solo dopo aver convertito il nome e il cognome usando la funzione Maiuscolo    
+*/
 int main()
 {
     Persona_T persona;
